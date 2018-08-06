8c9dc7dca3845cb2f0c613588f736f0d248e1eed
Edward paulino xuluc chulim <edward.xuluc@creatibot.com>
2018-08-06 15:51:50 -0500

    commit inicial

---
+++ angular-form-label.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('angular.form.group', [])
+
+.directive('formLabel', function(){
+    return{
+        template : '<label ng-transclude=""></label>',
+        transclude : true,
+        restrict: 'E',
+        scope: {
+            form : '=',
+            name : '@',
+        },
+        link: function($scope, elemnt, iAttrs, controller) {
+            if( $scope.form && $scope.name ){
+                $scope.$watch("form['" + $scope.name + "']", function ( input ){
+                    if( input && input.$validators ){
+                        if( input.$validators.required && !input.$validators.required() ){
+                            elemnt.find('label').append('<span class="text-danger">*</span>');
+                        }
+                    }
+                }, true);
+            }
+        }
+    };
+})
