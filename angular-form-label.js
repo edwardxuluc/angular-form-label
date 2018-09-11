91625e567e61140de3a569df7434d44b32a78f85
Edward paulino xuluc chulim <edward.xuluc@creatibot.com>
2018-09-11 10:46:28 -0500

    nueva version form label

--- angular-form-label.js
+++ angular-form-label.js
@@ -3,23 +3,27 @@
 angular.module('angular.form.label', [])
 
 .directive('formLabel', function(){
-    return{
-        template : '<label ng-transclude=""></label>',
-        transclude : true,
-        restrict: 'E',
-        scope: {
-            form : '=',
-            name : '@',
-        },
-        link: function($scope, elemnt, iAttrs, controller) {
-            if( $scope.form && $scope.name ){
-                $scope.$watch("form['" + $scope.name + "']", function ( input ){
-                    if( input && input.$validators ){
-                        if( input.$validators.required && !input.$validators.required() ){
-                            elemnt.find('label').append('<span class="text-danger">*</span>');
-                        }
-                    }
-                }, true);
+    return {
+        restrict: 'A',
+        link: function($scope, element, iAttrs, controller ) {
+            var element      = angular.element( element );
+            var formName     = element.parents('[ng-form]').length ? element.parents('[ng-form]').attr('ng-form') : element.parents('form').attr('name');
+            var inputName    = element.parent().find('.form-control').attr('name');
+            var inputElement = element.parent().find('.form-control');
+
+            if( formName && inputElement ){
+                
+                var required = false;
+
+                if( inputElement.attr('required') ){
+                    required = true;
+                }else if( inputElement.attr('ng-required') && $scope.$eval(inputElement.attr('ng-required')) ){
+                    required = true;
+                }
+
+                if( required && element.find('span').length == 0 ){
+                    element.append('<span class="text-danger">*</span>');
+                }
             }
         }
     };
